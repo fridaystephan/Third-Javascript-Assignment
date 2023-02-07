d1b8b3dc4a4540b7921a9307e58fba6a3959bc72
stephen <fridaystephan@gmail.com>
2023-02-07 20:09:47 +0100

    My First Commit

---
+++ script.js
@@ -0,0 +1,31 @@
+//Question 1
+let arr = [[1, 2, 3],[4, 5, 6],7,8,9,[[10, 11, 12, [13, 4, 112]], 136, 14],];
+
+console.log(arr[3]); 
+console.log(arr[5][0][3][0]);
+console.log(arr[5][1]); 
+console.log(arr[5][0][3][1]);
+
+//Question 2
+var sentence = ["javascript", "web", "Capacity Bay"];
+console.log("I am a " + sentence[0] + " developer at " + sentence[1] + " and I love " + sentence[2] + ".");
+
+//Question 3
+var str = "Generalization";
+console.log(str.slice(3, str.length));
+
+//Question 4
+var fruits = ["orange", "apple", "guava", "lemon"];
+
+// Adding elements at the beginning of the array
+fruits.unshift("pea", "banana");
+
+// Adding elements at the end of the array
+fruits.push("Avocado", "mango");
+
+console.log(fruits); 
+
+//Question 5
+var str = "Hello World";
+console.log(str.slice(0, 5)); 
+console.log(str.slice(6)); 
